2ad59c81917da2b38315632f84385ab55308319c
unzzui <diegobravobe@gmail.com>
2025-06-25 23:51:11 -0400

    minor

--- static/js/config.js
+++ static/js/config.js
@@ -124,7 +124,7 @@ function getBusinessParams() {
     
     // Patrones de crecimiento decreciente predefinidos
     // const trafficGrowthPattern = [1.00, 0.80, 0.50, 0.30, 0.20]; // Para años 2025-2030
-    const trafficGrowthPattern = [1.00, 1.00, 0.80, 0.70, 0.60]; // Para años 2025-2030
+    const trafficGrowthPattern = [1.00, 1.00, 1.00, 0.70, 0.60]; // Para años 2025-2030
 
     const conversionGrowthPattern = [0.40, 0.25, 0.15, 0.10, 0.05]; // Para años 2025-2030
 
